45159df13284facfb2cd36a19654cf9b18ef7b7e
AvivHK <Avivhkn@gmail.com>
2020-02-23 15:31:24 +0200

    fixing server and api

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,43 +1,45 @@
 const express = require('express')
 const router = express.Router()
-const request = require('request')
 const Lottery = require('../models/Lottery')
 
 
-router.get('lottery/:isProduct', async (req, res) => {
+
+router.get('/lottery/:isProduct', async (req, res) => {
     const { isProduct } = req.params
     if (isProduct) {
-        Lottery.find({ $and: [{ isProduct: { $e: true } }, { done: { e: false } }] }).exec((err, lotteries) => {
+        await Lottery.find({ $and: [{ isProduct: { $e: true } }, { done: { $e: false } }] }).exec((err, lotteries) => {
             res.send(lotteries)
         })
     }
     else {
-        Lottery.find({ $and: [{ isProduct: { $e: false } }, { done: { e: false } }] }).exec((err, lotteries) => {
+        await Lottery.find({ $and: [{ isProduct: { $e: false } }, { done: { $e: false } }] }).exec((err, lotteries) => {
             res.send(lotteries)
         })
     }
 })
 
-router.put('lottery/:lotteryID', async (req, res) => {
+router.put('/lottery/:lotteryID', async (req, res) => {
     const { lotteryID } = req.params;
-    let userBody = JSON.parse(result.userBody)
-    Lottery.findById(lotteryID)
+    let userBody = JSON.parse(req.userBody)
+    await Lottery.findById(lotteryID)
         .exec(function (err, response) {
             response.users.push(userBody)
         })
 })
 
-router.get('lottery/winners', async (req, res) => {
+router.get('/lottery/winners', async function (req, res) {
+    console.log(`jjhdjfh`);
     const winners = []
-    Lottery.find({ done: { $e: true } })
+    await Lottery.find({ done: { $e: true } })
         .exec(function (err, response) {
             response.forEach(u => winners.push(u));
             res.send(winners)
-        })    
+        })
 })
 
-router.post('lottery/newLottery', async (req, res) => {
-    let tempLottery = JSON.parse(req.tempLottery)
+router.post('/lottery/newLottery', async (req, res) => {
+    let tempLottery = req.body
+    console.log(tempLottery);
     let newLottery = new Lottery({
         entryFee: tempLottery.entryFee,
         isProduct: tempLottery.isProduct,
@@ -53,7 +55,8 @@ router.post('lottery/newLottery', async (req, res) => {
         winner: tempLottery.winner,
         done: tempLottery.done
     })
-    newLottery.save()
+    await newLottery.save()
+    res.send(newLottery)
 })
 
 module.exports = router
